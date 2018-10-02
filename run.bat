cecf247e0202281c821c6288f448eddc069778a6
donjor <donjordev@gmail.com>
2018-10-02 19:16:06 +0200

    updated default files

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ call:myDosFunc
 
 :myDosFunc
 	START /I DonjorNight-Autominer
-	TIMEOUT 7400
+	TIMEOUT 14400
 	TASKKILL /IM DonjorNight-Autominer.exe /F /T
 	TASKKILL /IM chromedriver.exe /F /T
 	TASKKILL /IM cast_xmr-vega.exe /F /T
